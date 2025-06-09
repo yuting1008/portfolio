3d4ddd0afd0fd661d434fb975332b34ccec08d14
Yuting <chenyuting1008@gmail.com>
2025-06-10 00:19:26 +0800

    feat: add navigation

---
+++ components/navigation.tsx
@@ -0,0 +1,88 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import { ModeToggle } from "@/components/mode-toggle";
+
+const links = [
+  {
+    path: "/",
+    title: "Home",
+  },
+  {
+    path: "/about",
+    title: "About",
+  },
+  {
+    path: "/projects",
+    title: "Projects",
+  },
+  {
+    path: "/resume",
+    title: "Resume",
+  },
+] as const;
+
+export default function Navigation() {
+  const pathname = `/${usePathname().split("/")[1]}`;
+
+  return (
+    <header className="md:mt-6">
+      <nav className="mx-auto flex max-w-[700px] items-center justify-between gap-3 px-4 py-3 md:px-6">
+        <Link href="/" className="hidden shrink-0 text-primary md:block">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            viewBox="0 0 375 375"
+            version="1.2"
+          >
+            <defs>
+              <clipPath id="clip1">
+                <path d="M 103.636719 64.171875 L 271.476562 64.171875 L 271.476562 310 L 103.636719 310 Z M 103.636719 64.171875 " />
+              </clipPath>
+            </defs>
+            <g id="surface1">
+              <g clipPath="url(#clip1)" clipRule="nonzero">
+                <path
+                  fill="currentColor"
+                  d="M 211.375 151.808594 C 203.050781 149.433594 194.765625 155.722656 194.765625 164.390625 L 194.765625 297.785156 C 194.765625 305.234375 201.695312 310.808594 208.941406 309.09375 C 244.675781 300.671875 271.355469 268.492188 271.355469 230.195312 C 271.355469 193.117188 246.363281 161.785156 211.375 151.808594 Z M 180.15625 144.734375 L 180.15625 297.785156 C 180.15625 305.234375 173.230469 310.808594 165.980469 309.09375 C 130.246094 300.675781 103.566406 268.492188 103.566406 230.195312 L 103.566406 74.296875 C 103.566406 68.449219 108.902344 64.046875 114.640625 65.164062 C 151.925781 72.410156 180.15625 105.335938 180.15625 144.734375 Z M 180.15625 144.734375 "
+                />
+              </g>
+            </g>
+          </svg>
+        </Link>
+
+        <div className="flex gap-1">
+          {links.map((link) => (
+            <Link
+              key={link.path}
+              href={link.path}
+              className={`${
+                pathname === link.path
+                  ? "text-stone-950 dark:text-neutral-50"
+                  : "text-gray-400 dark:text-gray-500"
+              } relative rounded-lg px-3 py-1.5 text-sm hover:text-stone-950 dark:hover:text-neutral-50 transition-colors`}
+              style={{
+                WebkitTapHighlightColor: "transparent",
+              }}
+            >
+              {pathname === link.path && (
+                <motion.span
+                  layoutId="bubble"
+                  className="absolute inset-0 -z-10 rounded-lg bg-gray-100 dark:bg-neutral-800"
+                  transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                />
+              )}
+              {link.title}
+            </Link>
+          ))}
+        </div>
+
+        <div className="flex h-8 w-8 items-center justify-center">
+          <ModeToggle />
+        </div>
+      </nav>
+    </header>
+  );
+}
