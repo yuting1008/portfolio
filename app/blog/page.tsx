ab7ce8a827de2cb6b71c647da6327b21dadec5c7
Yuting <chenyuting1008@gmail.com>
2025-07-03 16:23:14 +0800

    feat: add temporary text

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,9 @@
 export default function BlogPage() {
   return (
-    <header className="md:mt-6">
-      <nav className="mx-auto flex flex-col max-w-[700px] items-start justify-between gap-12 px-4 py-3 md:px-6">
-        <section className="flex flex-col gap-3"></section>
-      </nav>
+    <header className="md:mt-6 flex items-center justify-center">
+      <section className="text-center text-xl font-semibold text-gray-500">
+        🚧 Work in progress...
+      </section>
     </header>
   );
 }
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div></div>;
+export default function ProjectPage() {
+  return (
+    <header className="md:mt-6 flex items-center justify-center">
+      <section className="text-center text-xl font-semibold text-gray-500">
+        🚧 Work in progress...
+      </section>
+    </header>
+  );
 }
