e08ccdb4c5635d7b7da8655642e26744a6da521b
Yuting <chenyuting1008@gmail.com>
2025-07-03 15:52:26 +0800

    fix: update introduction

--- app/page.tsx
+++ app/page.tsx
@@ -103,13 +103,20 @@ export default function page() {
 
   return (
     <header className="md:mt-6">
-      <nav className="mx-auto flex flex-col max-w-[700px] items-start justify-between gap-8 px-4 py-3 md:px-6">
-        <section>
+      <nav className="mx-auto flex flex-col max-w-[700px] items-start justify-between gap-12 px-4 py-3 md:px-6">
+        <section className="flex flex-col gap-3">
           <h1 className="text-4xl font-bold mb-2">Hi, this is Yu-Ting Chen!</h1>
+
           <div>
-            I am an undergraduate in National Taiwan University majoring in
-            Information Management.
+            I am <span className="font-semibold">Yu-Ting Chen 陳郁婷</span> from{" "}
+            <span className="font-semibold">Taiwan</span>, and you can also call
+            me <span className="font-semibold">Sally</span>. Currently studying{" "}
+            <span className="font-semibold">Information Management</span> at{" "}
+            <span className="font-semibold">National Taiwan University</span>, I
+            bring a hybrid expertise in business and technology to solve complex
+            real-world challenges.
           </div>
+
           <div className="flex items-center gap-3 mt-2">
             <EmailButton />
 
